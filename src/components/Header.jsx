39dedc9203ed4cf98b213cb8c26c4f4f5a391fd3
Anup <anupbasnet057@gmail.com>
2023-02-14 23:25:44 +0545

    feat: Making user state persist on refresh
    User stays logged in while refreshing and added animation on clicking user avatar icon.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import { MdShoppingBasket } from 'react-icons/md'
+import React, { useState } from 'react'
+import { MdShoppingBasket, MdAdd, MdLogout } from 'react-icons/md'
 import { motion } from 'framer-motion'
-import { Link } from 'react-router-dom'
+import { json, Link } from 'react-router-dom'
 import Avatar from '/src/assets/avatar.png'
 import { useStateValue } from '../context/StateProvider';
 
@@ -15,16 +15,23 @@ const Header = () => {
 
     const [{user}, dispatch] = useStateValue()
 
+    const [isMenu, setIsMenu] = useState(false)
+
     const login = async () => {
-        const {user : {refreshToken, providerData}} = await signInWithPopup(firebaseAuth, provider)
+        if(!user){
+            const {user : {refreshToken, providerData}} = await signInWithPopup(firebaseAuth, provider)
         dispatch({
             type: actionType.SET_USER,
             user: providerData[0]
-        })
+        });
+        localStorage.setItem('user', JSON.stringify(providerData[0]))
+        } else {
+            setIsMenu(!isMenu);
+        }
     }
 
     return (
-        <header className='fixed z-50 w-screen  p-6 px-16'>
+        <header className='fixed z-50 w-screen  p-3 px-4 md:p-6 md:px-16'>
             {/* { desktop and tablet } */}
             <div className='hidden md:flex w-full h-full items-center justify-between'>
                 <Link to={'/'} className="flex items-center gap-2">
@@ -33,12 +40,16 @@ const Header = () => {
                 </Link>
 
                 <div className='flex items-center gap-8'>
-                    <ul className='flex items-center gap-8 ml-auto'>
+                    <motion.ul
+                        initial={{opacity:0, x: 200}}
+                        animate={{opacity:1, x: 0}}
+                        exit={{opacity:0, x: 200}}
+                        className='flex items-center gap-8 ml-auto'>
                         <li className='text-base text-textColor hover:text-headingColor duration-100 transistion-all ease-in-out cursor-pointer'>Home</li>
                         <li className='text-base text-textColor hover:text-headingColor duration-100 transistion-all ease-in-out cursor-pointer'>Genres</li>
                         <li className='text-base text-textColor hover:text-headingColor duration-100 transistion-all ease-in-out cursor-pointer'>About Us</li>
                         <li className='text-base text-textColor hover:text-headingColor duration-100 transistion-all ease-in-out cursor-pointer'>Services</li>
-                    </ul>
+                    </motion.ul>
 
                     <div className="relative flex items-center justify-center">
                         <MdShoppingBasket className='text-textColor text-2xl cursor-pointer' />
@@ -54,6 +65,26 @@ const Header = () => {
                             className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-2xl cursor-pointer rounded-full' alt="user avatar"
                             onClick={login}
                         />
+                        {
+                            isMenu && (
+                                <motion.div 
+                                    initial={{opacity: 0, scale: 0.6}}
+                                    animate={{opacity: 1, scale: 1}}
+                                    exit={{opacity: 0, scale: 0.6}}
+
+                                    className='w-40 bg-gray-50 shadow-xl rounded-lg absolute top-14 right-0 flex flex-col '>
+                            {
+                                user && user.email === 'euzaanc97@gmail.com' && (
+                                    <Link to={'/createItem'}>
+                                    <p className='px-4 py-2 flex items-center gap-2 cursor-pointer hover:bg-slate-100 transistion-all duration-100 ease-in-out text-textColor text-base'>New Item <MdAdd /></p>
+                                    </Link>
+                                )
+                            }
+                            
+                            <p className='px-4 py-2 flex items-center gap-2 cursor-pointer hover:bg-slate-100 transistion-all duration-100 ease-in-out text-textColor text-base'>Logout <MdLogout /></p>
+                        </motion.div>
+                            )
+                        }
                     </div>
 
                 </div>
@@ -61,7 +92,10 @@ const Header = () => {
 
             {/* mobile */}
             <div className="flex md:hidden w-full h-full">
-                Header
+                <Link to={'/'} className="flex items-center gap-2">
+                    <img src='src/assets/logo.jpg' className='h-8 object-cover' alt="logo" />
+                    <p className='text-headingColor text-xl font-bold'>KitabKhana</p>
+                </Link>
             </div>
 
 
