e3fadf283c51450ff623fa29ec55416a01cc6a6e
Anup <anupbasnet057@gmail.com>
2023-02-19 18:56:56 +0545

    feat:created form to add new book items
    New page for admin to add new book items information @ /createItem

---
+++ src/utils/data.js
@@ -0,0 +1,78 @@
+const data = [
+    {
+        id: 1,
+        title: "The Alchemist",
+        author: "Paulo Coelho",
+        genre: "fiction",
+        imageSrc: "https://m.media-amazon.com/images/I/81DWUPhkshL.jpg",
+        price: 529,
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, veritatis harum voluptatibus molestiae nihil commodi nobis quibusdam magnam ex labore assumenda adipisci ipsa quisquam aliquid excepturi tempora? Inventore facere a accusantium esse.",
+        rating: 4.2,
+    },
+    {
+        id: 2,
+        title: "The Inferno",
+        author: "Dan Brown",
+        genre: "fiction",
+        imageSrc: "https://m.media-amazon.com/images/I/81DWUPhkshL.jpg",
+        price: 729,
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, veritatis harum voluptatibus molestiae nihil commodi nobis quibusdam magnam ex labore assumenda adipisci ipsa quisquam aliquid excepturi tempora? Inventore facere a accusantium esse.",
+        rating: 4.4,
+    },
+    {
+        id: 3,
+        title: "It starts with us",
+        author: "Coolen Hoover",
+        genre: "non-fiction",
+        imageSrc: "https://m.media-amazon.com/images/I/81DWUPhkshL.jpg",
+        price: 659,
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, veritatis harum voluptatibus molestiae nihil commodi nobis quibusdam magnam ex labore assumenda adipisci ipsa quisquam aliquid excepturi tempora? Inventore facere a accusantium esse.",
+        rating: 4.3,
+    },
+]
+
+export const categories = [
+    {
+        id: 1,
+        name: "Fiction",
+        urlParamName: "fiction",
+    },
+    {
+        id: 2,
+        name: "Non-Fiction",
+        urlParamName: "non-fiction",
+    },
+    {
+        id: 3,
+        name: "Classics",
+        urlParamName: "classics",
+    },
+    {
+        id: 4,
+        name: "Erotica",
+        urlParamName: "erotica",
+    },
+    {
+        id: 5,
+        name: "Fantasy",
+        urlParamName: "fantasy",
+    },
+    {
+        id: 6,
+        name: "Horror",
+        urlParamName: "horror",
+    },
+
+    {
+        id: 7,
+        name: "Romance",
+        urlParamName: "romance",
+    },
+    {
+        id: 8,
+        name: "Biographies",
+        urlParamName: "biographies",
+    },
+];
+
+export default data;
